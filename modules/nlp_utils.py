760f76af6ed266279c7c4c27997d1356202c77ca
Yuan Shi <shi17@upenn.edu>
2025-12-11 23:04:17 -0500

    Create nlp_utils.py for NLP analysis functions
    This module provides basic NLP analysis functions for cat sounds or user notes.

---
+++ modules/nlp_utils.py
@@ -0,0 +1,12 @@
+"""
+nlp_utils.py
+Module for basic NLP analysis on cat sounds or user notes.
+"""
+
+def analyze_text(text):
+    """Perform simple NLP analysis (e.g., keywords, sentiment)"""
+    pass
+
+def summarize_notes(notes_list):
+    """Summarize multiple notes"""
+    pass
