88b7984c8f391292045229583e813cb7e5739d0b
Yuan Shi <shi17@upenn.edu>
2025-12-11 23:03:56 -0500

    Add visualization module for generating charts
    Created visualization module with chart plotting functions.

---
+++ modules/visualization.py
@@ -0,0 +1,17 @@
+"""
+visualization.py
+Module for generating charts using Matplotlib/Plotly.
+"""
+import matplotlib.pyplot as plt
+
+def plot_water_level(df):
+    """Line chart for water level"""
+    pass
+
+def plot_feeding(df):
+    """Bar chart for feeding events"""
+    pass
+
+def plot_cat_presence(df):
+    """Pie chart for cat presence"""
+    pass
