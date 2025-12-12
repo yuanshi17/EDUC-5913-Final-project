08f71b6873792f7614cd19369685dbeff41a7d57
Yuan Shi <shi17@upenn.edu>
2025-12-11 23:03:33 -0500

    Add api_utils.py for API interactions
    Created a module for external API calls related to cat camera and health information.

---
+++ modules/api_utils.py
@@ -0,0 +1,12 @@
+"""
+api_utils.py
+Module for external API calls, e.g., cat camera or health info.
+"""
+
+def get_cat_camera_feed():
+    """Fetch cat camera feed (simulated)"""
+    pass
+
+def get_health_info(cat_id):
+    """Fetch health info from API"""
+    pass
