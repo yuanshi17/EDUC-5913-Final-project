6f5d7443b37e7392e700e7f64f77af3412a8916c
Helena He <hhe123@upenn.edu>
2025-12-14 21:49:43 -0500

    Add files via upload

--- streamlit_app/main.py
+++ streamlit_app/main.py
@@ -2,12 +2,12 @@
 import streamlit as st
 from streamlit_app.Home import home_page
 from streamlit_app.Health_Monitor import health_monitor_page
-from streamlit_app.Food_Tracker import food_tracker_page
+from streamlit_app.Food_and_Water_Tracker import food_and_water_tracker_page
 from streamlit_app.Behavior_Analysis import behavior_analysis_page
 
 st.set_page_config(page_title="🐱 Virtual Cat Monitor", layout="centered")
 
-# 侧边栏导航
+# Sidebar Nevigation
 st.sidebar.title("Navigation")
 page = st.sidebar.selectbox("Go to", ["Home", "Health Monitor", "Food Tracker", "Behavior Analysis"])
 
@@ -15,7 +15,7 @@ if page == "Home":
     home_page()
 elif page == "Health Monitor":
     health_monitor_page()
-elif page == "Food Tracker":
-    food_tracker_page()
+elif page == "Food and Water Tracker":
+    food_and_water_tracker_page()
 elif page == "Behavior Analysis":
     behavior_analysis_page()
